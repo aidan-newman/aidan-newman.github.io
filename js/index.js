ed09b08e2aa88828b6eeb948b1f0402eb54a4be6
Aidan Newman <aidannewm@gmail.com>
2024-04-18 17:03:28 -0400

    fixed garbage js

--- js/index.js
+++ js/index.js
@@ -1,56 +1,65 @@
 
-window.addEventListener("load", function(){
-
-    const gap_divs = document.getElementsByClassName("gap_div");
-    const pages = document.getElementsByClassName("header_page");
-    let current = document.getElementById("startup");
+let gap_divs;
+let pages;
+let current;
 
 
+function highlightGaps(enter, i) {
+    if (enter) { // mouse enter (highlight)
+        if(pages[i] === current) {return;}
+        if (i>=0) {
+            gap_divs[i].style.borderBottomRightRadius = "10px";
+        }
+        if (i+1<gap_divs.length) {
+            gap_divs[i+1].style.borderBottomLeftRadius = "10px";
+        }
+    } else { // mouse exit (unhighlight)
+
+        if(pages[i] === current) {return;}
+        if (i>=0) {
+            gap_divs[i].style.borderBottomRightRadius = "0";
+        }
+        if (i+1<gap_divs.length) {
+            gap_divs[i + 1].style.borderBottomLeftRadius = "0";
+        }
+    }
+}
+
+function main() {
+
+    gap_divs = document.getElementsByClassName("gap_div");
+    pages = document.getElementsByClassName("header_page");
+    current = document.getElementById("startup");
 
     for (let i = 0; i < pages.length; i++) {
 
         if (pages[i] === current) {
             if (i>=0) {
-                gap_divs[i-1].style.borderRadius = "0 0 10px 0";
+                gap_divs[i].style.borderRadius = "0 0 10px 0";
             }
             if (i+1<gap_divs.length) {
-                gap_divs[i].style.borderRadius = "0 0 0 10px";
+                gap_divs[i+1].style.borderRadius = "0 0 0 10px";
             }
             pages[i].style.borderRadius = "10px 10px 0 0";
             pages[i].style.backgroundColor = "#ECF0F1";
         }
 
-        pages[i].addEventListener("mouseover", function() {
-            if(pages[i] === current) {return;}
-            if (i>=0) {
-                gap_divs[i-1].style.borderBottomRightRadius = "10px";
-            }
-            if (i+1<gap_divs.length) {
-                gap_divs[i].style.borderBottomLeftRadius = "10px";
-            }
-        });
+        pages[i].addEventListener("mouseover", function(){highlightGaps(true, i)});
+        pages[i].addEventListener("mouseleave", function(){highlightGaps(false, i)});
+
         pages[i].addEventListener("click", function() {
-            console.log("click");
             current = pages[i];
 
             if (i>=0) {
-                gap_divs[i-1].style.borderRadius = "0 0 10px 0";
+                gap_divs[i].style.borderRadius = "0 0 10px 0";
             }
             if (i+1<gap_divs.length) {
-                gap_divs[i].style.borderRadius = "0 0 0 10px";
+                gap_divs[i+1].style.borderRadius = "0 0 0 10px";
             }
             pages[i].style.borderRadius = "10px 10px 0 0";
             pages[i].style.backgroundColor = "#ECF0F1";
         });
-
-        pages[i].addEventListener("mouseleave", function() {
-            if(pages[i] === current) {return;}
-            if (i>=0) {
-                gap_divs[i-1].style.borderBottomRightRadius = "0";
-            }
-            if (i+1<gap_divs.length) {
-                gap_divs[i].style.borderBottomLeftRadius = "0";
-            }
-        });
     }
-});
+}
+
+window.addEventListener("load", main);
